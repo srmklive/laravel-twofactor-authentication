cf6703ed454ed1a3d7ef93fb6bf02024977a3abd
Raza Mehdi <srmk@outlook.com>
2015-12-17 19:53:27 +0500

    Added bootstrap to authy registration view

--- views/form.blade.php
+++ views/form.blade.php
@@ -1,20 +1,46 @@
 @section('styles')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/authy-forms.css/2.2/flags.authy.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/authy-forms.css/2.2/form.authy.css" />
 @endsection
 
-<div class="login-form">
+@section('content')
+<div class="col-sm-4 col-sm-offset-4">
     <form method="POST">
         {{csrf_field()}}
         <h3>Enable Two-Factor Authentication</h3>
-        Country:
-        <select id="authy-countries" name="country-code"></select>
-        <br/>
-        Cellphone: <input id="authy-cellphone" type="text" value="" name="authy-cellphone" />
-        <button type="submit">Submit</button>
+        <div class="row">
+            <div class="col-xs-3">Country:</div>
+            <div class="col-xs-9">
+                <select id="authy-countries" name="country-code"></select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-3">Cellphone:</div>
+            <div class="col-xs-9">
+                <input id="authy-cellphone" type="text" value="" name="authy-cellphone" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-9 col-xs-offset-3">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_sms" />
+                        Send two-factor token via SMS
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-9 col-xs-offset-3">
+                <button type="submit">Submit</button>
+            </div>
+        </div>        
     </form>
 </div>
+@endsection
 
 @section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/authy-forms.js/2.2/form.authy.js"></script>
 @endsection
